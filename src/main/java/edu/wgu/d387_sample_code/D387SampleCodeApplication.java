34e58fb6833bbe0bfaf3dca9713dfe4d3893fa62
Evan Meserve <emeser3@wgu.edu>
2025-07-06 16:16:16 -0700

    worked on displaying the welcome message in both English and French by applying the resource bundles using a different thread for each language. Not showing on front end yet.

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -5,25 +5,50 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
+import java.util.concurrent.ExecutorService;
+
 
 import java.io.InputStream;
 import java.util.Properties;
 
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
 @SpringBootApplication
 public class D387SampleCodeApplication {
-
+	public static String fromThread1;
+	public static String fromThread2;
+	static ExecutorService messageExecutor = newFixedThreadPool(10);
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-
 		Properties properties=new Properties();
-		try{
-			InputStream stream = new ClassPathResource("welcome_en_US.properties").getInputStream();
-			properties.load(stream);
-			System.out.println(properties.getProperty("welcome"));
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
+		messageExecutor.execute(()-> {
+			try{
+				InputStream stream = new ClassPathResource("welcome_en_US.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcome"));
+				fromThread1 = properties.getProperty("welcome");
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		});
+
+		messageExecutor.execute(()-> {
+			try{
+				InputStream stream = new ClassPathResource("welcome_fr_CA.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcome"));
+				fromThread2 = properties.getProperty("welcome");
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+		});
+
+		//System.out.println(fromThread1);
+		//System.out.println(fromThread2);
+
 	}
 
 }
